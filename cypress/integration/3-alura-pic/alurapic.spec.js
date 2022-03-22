b4179fe07cdeb13453f8583722c35c4287fb6c76
eddcp <eduardocspv@gmail.com>
2022-03-22 19:06:52 -0300

    Improving test suite

--- cypress/integration/3-alura-pic/alurapic.spec.js
+++ cypress/integration/3-alura-pic/alurapic.spec.js
@@ -11,7 +11,28 @@ describe('Login and signup of users on alura pic', () => {
         cy.contains('ap-vmessage', 'User name is required!').should('be.visible');
         cy.contains('ap-vmessage', 'Full name is required!').should('be.visible');
         cy.contains('ap-vmessage', 'Password is required!').should('be.visible');
+    })
 
+    it('verify invalid email message', () => {
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="email"]').type('eduardo');
+        cy.contains('ap-vmessage', 'Invalid e-mail').should('be.visible');
+    })
+    
+    it('verify minimun 8 length password', () => {
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="password"]').type('123');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Mininum length is 8').should('be.visible');
+    })
 
+    it('verify username lower case', () => {
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="userName"]').type('TEST');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Must be lower case').should('be.visible');
     })
 })
