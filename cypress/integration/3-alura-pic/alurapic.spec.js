1e1719495df25dec7135c44511d13b2f349a96f0
eddcp <eduardocspv@gmail.com>
2022-03-24 17:03:07 -0300

    Adding command login

--- cypress/integration/3-alura-pic/alurapic.spec.js
+++ cypress/integration/3-alura-pic/alurapic.spec.js
@@ -35,4 +35,16 @@ describe('Login and signup of users on alura pic', () => {
         cy.contains('button', 'Register').click();
         cy.contains('ap-vmessage', 'Must be lower case').should('be.visible');
     })
+
+    it.only('login with valid user', () => {
+        cy.login('flavio', '123');
+        cy.contains('a', '(Logout').should('be.visible');
+    })
+
+    it.only('login with invalid user', () => {
+        cy.login('jacqueline', '1234');
+        cy.on ('window:alert', (str) => {
+            expect(str).to.equal('Invalid user name or password');
+        })
+    })
 })
---
+++ cypress/support/gui_commands.js
@@ -0,0 +1,5 @@
+Cypress.Commands.add('login', (nome, senha) => {
+    cy.get('input[formcontrolname="userName"]').type(nome);
+    cy.get('input[formcontrolname="password"]').type(senha);
+    cy.get('button[type="submit"]').click();
+})
